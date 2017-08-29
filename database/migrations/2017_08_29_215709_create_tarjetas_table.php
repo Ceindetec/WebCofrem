b14b1879305c9c584dbd8c9d388b80f905764de4
sofi876 <sofi876@hotmail.com>
2017-08-29 18:24:45 -0500

    comit prueba

---
+++ database/migrations/2017_08_29_215709_create_tarjetas_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTarjetasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tarjetas', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->primary('id');
+            $table->string('numero_tarjeta')->unique();
+            $table->enum('tipo',['A','R','B']);
+            $table->boolean('cambioclave')->default(false);
+            $table->string('password');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tarjetas');
+    }
+}
