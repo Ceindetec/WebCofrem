fee330acfae23812d3f3803171e53540eaa2e360
mdxshana <deiby.tovar@ceindetec.org.co>
2017-08-30 18:57:29 -0500

    agregada validacion de agregar establicimiento
    agregado editar establecimiento.

--- routes/web.php
+++ routes/web.php
@@ -92,6 +92,8 @@ Route::group(['middleware' => 'auth'], function () {
         route::get('establecimientos/gridestablecimientos','EstablecimientosController@gridEstablecimientos')->name('gridestablecimientos');
         route::get('establecimientos/crear','EstablecimientosController@viewCrearEstablecimiento')->name('establecimiento.crear');
         route::post('establecimientos/crear','EstablecimientosController@crearEstablecimiento')->name('establecimiento.crearp');
+        route::get('establecimientos/editar','EstablecimientosController@viewEditarEstablecimiento')->name('establecimiento.editar');
+        route::post('establecimientos/editar','EstablecimientosController@editarEstablecimiento')->name('establecimiento.editarp');
 
         /*FINALIZA ESTABLECIMIENTOS*/
 
