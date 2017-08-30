33d9d449ecac1c592bd6d614459bf722009a976e
sofi876 <sofi876@hotmail.com>
2017-08-30 15:49:39 -0500

    Modelos y migraciones TarjetasRegalo, Terceros (identificacion para las tarjetas bono) y TarjetasBono

---
+++ database/migrations/2017_08_30_203023_alter_tarjetas_regalo.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterTarjetasRegalo extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::table('tarjetas_regalos', function($table) {
+            $table->dropColumn('monto_inicial');
+            $table->dropColumn('monto_restante');
+        });
+        Schema::table('tarjetas_regalos', function($table) {
+            $table->double('monto_inicial',15,2)->nullable();
+            $table->double('monto_restante',15,2)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::table('tarjetas_regalos', function($table) {
+            $table->dropColumn('monto_inicial');
+            $table->dropColumn('monto_restante');
+        });
+    }
+}
