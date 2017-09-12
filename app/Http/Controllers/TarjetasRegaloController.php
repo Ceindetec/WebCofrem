8515ad54edb4e27564ecbe0cd023f19b7eed4ee2
Unknown <luis.pineda@ceindetec.org.co>
2017-09-12 15:04:32 -0500

    trabajando en la tarjeta regalo

---
+++ app/Http/Controllers/TarjetasRegaloController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace creditocofrem\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TarjetasRegaloController extends Controller {
+
+    public function crearTarjetaRegalo() {
+
+    }
+
+}
