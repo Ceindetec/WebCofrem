fb9680a386f849b583d484d6001a4216dfbc847c
mdxshana <deiby.tovar@ceindetec.org.co>
2017-09-28 16:55:45 -0500

    agregado ejemplo para los webservices

--- app/Http/Controllers/WebApiController.php
+++ app/Http/Controllers/WebApiController.php
@@ -7,4 +7,15 @@ use Illuminate\Http\Request;
 class WebApiController extends Controller
 {
     //
+
+    public function comunicacion(Request $request){
+        if($request->codigo == '02'){
+            $result['estado'] = true;
+            $result['mensaje'] = 'Comunicacion exitosa';
+        }else{
+            $result['estado'] = false;
+            $result['mensaje'] = 'Error de comunicacion';
+        }
+        return $result;
+    }
 }
