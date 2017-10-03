e8d7c829b329bcb401281f0a8fdab1ad365f243a
sofi876 <sofi876@hotmail.com>
2017-10-03 18:29:34 -0500

    actualizacion rutas

--- routes/ingrid.php
+++ routes/ingrid.php
@@ -14,9 +14,9 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function () {
 
-    Route::group(['middleware' => 'roleshinobi:admin'], function (){
 
-        /*INICIO TARJETAS*/
+
+
     /*INICIO TARJETAS*/
 
     route::get('tarjetas', 'TarjetasController@index')->name('tarjetas');
@@ -28,15 +28,7 @@ Route::group(['middleware' => 'auth'], function () {
     route::get('tarjetas/crearbloque', 'TarjetasController@viewCrearTarjetaBloque')->name('tarjetas.crearbloque');
     route::post('tarjetas/crearbloque', 'TarjetasController@crearTarjetaBloque')->name('tarjetas.crearbloquep');
     //route::get('tarjetas','TarjetasController@index')->name('tarjetas');
-        route::get('tarjetas','TarjetasController@index')->name('tarjetas');
-        route::get('tarjetas/gridtarjetas','TarjetasController@gridTarjetas')->name('gridtarjetas');
-        route::get('tarjetas/crear','TarjetasController@viewCrearTarjeta')->name('tarjetas.crear');
-        route::post('tarjetas/crear','TarjetasController@crearTarjeta')->name('tarjetas.crearp');
-        route::get('tarjetas/editar','TarjetasController@viewEditarTarjeta')->name('tarjetas.editar');
-        route::post('tarjetas/editar/{id}','TarjetasController@editarTarjeta')->name('tarjetas.editarp');
-        route::get('tarjetas/crearbloque','TarjetasController@viewCrearTarjetaBloque')->name('tarjetas.crearbloque');
-        route::post('tarjetas/crearbloque','TarjetasController@crearTarjetaBloque')->name('tarjetas.crearbloquep');
-        //route::get('tarjetas','TarjetasController@index')->name('tarjetas');
+
 
     /*FINALIZA TARJETAS*/
     /* INICIA CREACION TARJETAS BONO */
