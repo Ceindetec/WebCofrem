f2eeb8912075c6fe802241d972a0eae6276941e0
sofi876 <sofi876@hotmail.com>
2017-11-29 17:55:28 -0500

    * Consulta de Empresas en SevenAs: al crear y editar empresa.

--- resources/views/empresas/editarempresa.blade.php
+++ resources/views/empresas/editarempresa.blade.php
@@ -6,7 +6,85 @@
         <h4 class="modal-title">Editar empresa</h4>
     </div>
     <div class="modal-body">
+        @if($empresa->tipo=="A")
+            <div class="form-group">
+                <div class="widget-inline-box" align="center">
+                    <a href="#" onclick="consultarWS()" id="botonConsultar" data-modal class="btn btn-custom waves-effect waves-light" data-target="#modalrol">Actualizar con SevenAs</a>
+                </div>
+            </div>
+            <div name="mensaje" id="mensaje" style="visibility: hidden;"><font color="red">No existe en SevenAs</font></div>
+            <div name="mensajeok" id="mensajeok" style="visibility: hidden;"><font color="green">Actualizado con SevenAs</font></div>
+            <div class="form-group">
+                <label class="col-md-2 control-label">Tipo de documento</label>
+                <div class="col-md-4">
+                    {{Form::select("tipo_documento",$tipoDocu,null,['id'=>'tipo_documento', 'class'=>'form-control', "tabindex"=>"1", "onchange"=>"limpiar()", "readonly", "disabled"])}}
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-2 control-label">Número de documento</label>
+                <div class="col-md-10">
+                {{Form::text('nit', null ,['id'=>'nit','class'=>'form-control', "required", "maxlength"=>"10", "tabindex"=>"2", "onchange"=>"limpiar()", "readonly"])}} <!-- , "data-parsley-type"=>"number" -->
+                </div>
+            </div>
 
+            <div class="form-group">
+                <label class="col-md-2 control-label">Razón social</label>
+                <div class="col-md-10">
+                    {{Form::text('razon_social', null ,['id'=>'razon_social', 'class'=>'form-control', "required", "maxlength"=>"40", "data-parsley-pattern"=>"^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]+(\s*[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]*)*[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]+$", "tabindex"=>"3", "readonly"])}}
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-2 control-label">Representante Legal</label>
+                <div class="col-md-10">
+                    {{Form::text('representante_legal', null ,['id'=>'representante_legal', 'class'=>'form-control', "required", "maxlength"=>"40", "data-parsley-pattern"=>"^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]+(\s*[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]*)*[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]+$", "tabindex"=>"4", "readonly"])}}
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-2 control-label">Departamento</label>
+                <div class="col-md-4">
+                    {{Form::select("departamento_codigo",$departamentos,null,['class'=>'form-control', "tabindex"=>"5", 'id'=>'departamento'])}}
+                </div>
+                <label class="col-md-2 control-label">Ciudad</label>
+                <div class="col-md-4">
+                    <select name="municipio_codigo" id="municipio" tabindex="6" class="form-control">
+                        <option>Seleccione...</option>
+                    </select>
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-2 control-label">Direccion</label>
+                <div class="col-md-10">
+                    {{Form::text('direccion', null ,['id'=>'direccion', 'class'=>'form-control', "required", "maxlength"=>"40", "tabindex"=>"7", "readonly"])}}
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-2 control-label">E-mail</label>
+                <div class="col-md-10">
+                    {{Form::email('email', null ,['id'=>'email', 'class'=>'form-control', "required", "tabindex"=>"8", "readonly"])}}
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-2 control-label">Telefono</label>
+                <div class="col-md-10">
+                    {{Form::text('telefono', null ,['id'=>'telefono', 'class'=>'form-control', "required", "data-parsley-type"=>"number", "maxlength"=>"10", "tabindex"=>"9", "readonly"])}}
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-2 control-label">Celular</label>
+                <div class="col-md-10">
+                    {{Form::text('celular', null ,['id'=>'celular', 'class'=>'form-control', "required", "data-parsley-type"=>"number", "maxlength"=>"10", "tabindex"=>"10", "readonly"])}}
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="col-md-2 control-label">Tipo</label>
+                <div class="col-md-10">
+                    <!-- { {Form::select('tipo', [ 'T' => 'Tercero', 'A' => 'Afiliado'], 'T',['id'=>'tipo', 'class'=>'form-control', "tabindex"=>"11", "required", "readonly", "onmouseover"=>"this.disabled=true", "onmouseout"=>"this.disabled=false"])}} -->
+                    {{Form::text('tipo_name', 'Afiliado' ,['id'=>'tipo_name', 'class'=>'form-control', "required", "readonly", "tabindex"=>"11"])}}
+
+                </div>
+            </div>
+        @endif
+        @if($empresa->tipo=="T")
         <div class="form-group">
             <label class="col-md-2 control-label">Tipo de documento</label>
             <div class="col-md-4">
@@ -19,15 +97,19 @@
                 {{Form::text('nit', null ,['class'=>'form-control', "id"=>"nit", "required", "tabindex"=>"1","maxlength"=>"10", "tabindex"=>"2"])}} <!-- , "data-parsley-type"=>"number" -->
             </div>
         </div>
-
-
+                <div class="form-group">
+                    <div class="widget-inline-box" align="center">
+                        <a href="#" onclick="consultarWS()" id="botonConsultar" data-modal class="btn btn-custom waves-effect waves-light" data-target="#modalrol">Consultar en SevenAs</a>
+                    </div>
+                </div>
+                <div name="mensaje" id="mensaje" style="visibility: hidden;"><font color="red">No existe en SevenAs</font></div>
+                <div name="mensajeok" id="mensajeok" style="visibility: hidden;"><font color="green">Actualizado con SevenAs</font></div>
         <div class="form-group">
             <label class="col-md-2 control-label">Razón social</label>
             <div class="col-md-10">
                 {{Form::text('razon_social', null ,['class'=>'form-control', "id"=>"razon_social", "required", "maxlength"=>"40", "data-parsley-pattern"=>"^[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]+(\s*[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]*)*[a-zA-Z0-9ñÑáéíóúÁÉÍÓÚ]+$", "tabindex"=>"3"])}}
             </div>
         </div>
-
         <div class="form-group">
             <label class="col-md-2 control-label">Representante Legal</label>
             <div class="col-md-10">
@@ -73,9 +155,11 @@
         <div class="form-group">
             <label class="col-md-2 control-label">Tipo</label>
             <div class="col-md-10">
-                {{Form::select('tipo', ['T' => 'Tercero', 'A' => 'Afiliado'], 'T', ['class'=>'form-control', "tabindex"=>"11", "required"])}}
+                <!-- { {Form::select('tipo', ['T' => 'Tercero', 'A' => 'Afiliado'], 'T', ['class'=>'form-control', "tabindex"=>"11", "required"])}} -->
+                {{Form::text('tipo_name', "Tercero" ,['id'=>'tipo_name', 'class'=>'form-control', "required", "readonly", "tabindex"=>"11"])}}
             </div>
         </div>
+            @endif
 
     </div>
     <div class="modal-footer">
@@ -84,7 +168,6 @@
     </div>
     {{Form::close()}}
 </div>
-
 
 
 <script>
@@ -178,5 +261,98 @@
 
         })
     }
+    function limpiar() {
+        document.getElementById("mensaje").style.visibility = "hidden";
+        document.getElementById("mensajeok").style.visibility = "hidden";
+        //document.getElementById('tipo').selectedIndex = "T";
+
+        $('#tipo_name').attr('readonly', true);
+        //$('#tipo option:not(:selected)').attr('disabled',true);
+        document.getElementById('tipo_name').value = "Tercero";
+        $('#razon_social').removeAttr('readonly');
+        $('#representante_legal').removeAttr('readonly');
+        $('#direccion').removeAttr('readonly');
+        $('#email').removeAttr('readonly');
+        $('#telefono').removeAttr('readonly');
+        $('#celular').removeAttr('readonly');
+        document.getElementById('razon_social').value = "";
+        document.getElementById('representante_legal').value = "";
+        document.getElementById('direccion').value = "";
+        document.getElementById('email').value = "";
+        document.getElementById('telefono').value = "";
+        document.getElementById('celular').value = "";
+    }
+    function consultarWS() {
+        var tipo = $("#tipo_documento").val();
+        var num = $("#nit").val();
+        $.ajax({
+            method: "get",
+            url: "{!!route('consultaraportante')!!}",
+            data: {
+                "tipo": tipo,
+                "num": num
+            },
+            error: function (data) {
+                //something went wrong with the request
+                alert("Error");
+            },
+            success: function (data) {
+
+                if(data.tipo=="A")
+                {
+                    document.getElementById("mensaje").style.visibility = "hidden";
+                    document.getElementById("mensajeok").style.visibility = "visible";
+                    document.getElementById('razon_social').value = data.razon_social;
+                    document.getElementById('representante_legal').value = data.representante_legal;
+                    document.getElementById('direccion').value = data.direccion;
+                    document.getElementById('email').value = data.email;
+                    document.getElementById('telefono').value = data.telefono;
+                    document.getElementById('celular').value = data.celular;
+                    document.getElementById('tipo_name').value = "Afiliado";//data.tipo
+                    document.getElementById('departamento').value = data.departamento_codigo;
+                    getMunicipios();
+                    document.getElementById('municipio').value = data.municipio_codigo;
+                    $('#razon_social').attr('readonly', true);
+                    $('#representante_legal').attr('readonly', true);
+                    $('#direccion').attr('readonly', true);
+                    if(data.email == "NA")
+                        $('#email').removeAttr('readonly');
+                    else
+                        $('#email').attr('readonly', true);
+                    $('#telefono').attr('readonly', true);
+                    if(data.celular == 0)
+                        $('#celular').removeAttr('readonly');
+                    else
+                        $('#celular').attr('readonly', true);
+                    $('#tipo_name').attr('readonly', true);
+                    //$('#tipo option:not(:selected)').attr('disabled',true);
+                    //$('#dias_consumo').removeAttr('disabled');
+                }
+                else
+                {
+                    //limpiar();
+                    document.getElementById("mensaje").style.visibility = "visible";
+                    document.getElementById("mensajeok").style.visibility = "hidden";
+                    document.getElementById('tipo_name').value = "Tercero";//data.tipo
+                }
+            }
+        });
+
+    }
+    function bloquear(){
+        $('#razon_social').attr('readonly', true);
+        $('#representante_legal').attr('readonly', true);
+        $('#direccion').attr('readonly', true);
+        if(document.getElementById('email').value == "NA")
+            $('#email').removeAttr('readonly');
+        else
+            $('#email').attr('readonly', true);
+        $('#telefono').attr('readonly', true);
+        if(document.getElementById('celular').value == 0)
+            $('#celular').removeAttr('readonly');
+        else
+            $('#celular').attr('readonly', true);
+        $('#tipo_name').attr('readonly', true);
+    };
 
 </script>
